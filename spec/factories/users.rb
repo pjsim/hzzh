60f3d15227b62491d403ab7e47a6ddf3b2061738
Phillip Simmonds <phil@Aarons-MacBook-Air.local>
2015-03-23 16:32:30 +1100

    first commit

---
+++ spec/factories/users.rb
@@ -0,0 +1,8 @@
+password = Faker::Internet.password(8)
+FactoryGirl.define do
+  factory :user, class: 'User' do
+    email { Faker::Internet.email }
+    password password
+    password_confirmation password
+  end
+end
